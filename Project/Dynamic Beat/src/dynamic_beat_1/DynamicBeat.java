b4a1abeb144afe36590392a204e3e72ddab6346a
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2019-02-05 23:14:24 +0900

    back ground
    back ground

--- Project/Dynamic Beat/src/dynamic_beat_1/DynamicBeat.java
+++ Project/Dynamic Beat/src/dynamic_beat_1/DynamicBeat.java
@@ -1,8 +1,16 @@
 package dynamic_beat_1;
 
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class DynamicBeat extends JFrame{
+	
+	private Image screenImage;
+	private Graphics screenGraphic;
+	private Image introBackground;
 	
 	public DynamicBeat() {
 		setTitle("Dynamic Beat");
@@ -11,5 +19,20 @@ public class DynamicBeat extends JFrame{
 		setLocationRelativeTo(null); //실행 했을 때 컴퓨터의 정중앙으로 뜨도록
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//게임창을 종료 했을 때 프로그램 모두 종료
 		setVisible(true); //창이 보이도록, 출력이 되도록
+		
+		introBackground = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
+	}
+	
+	//반복해서 화면을 띄움
+	public void paint(Graphics g) {
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		screenGraphic = screenImage.getGraphics();
+		screenDraw(screenGraphic);
+		g.drawImage(screenImage,0,0,null);
+	}
+	
+	public void screenDraw(Graphics g) {
+		g.drawImage(introBackground, 0, 0, null);
+		this.repaint();	//여기서 다시 반복
 	}
 }
