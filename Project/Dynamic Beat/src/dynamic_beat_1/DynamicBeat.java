0a30d0c3bee50b5544a6e1bdb31e3d79c7fc9609
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2019-02-07 21:49:45 +0900

    exit Button
    exit Button

--- Project/Dynamic Beat/src/dynamic_beat_1/DynamicBeat.java
+++ Project/Dynamic Beat/src/dynamic_beat_1/DynamicBeat.java
@@ -1,41 +1,108 @@
 package dynamic_beat_1;
 
+import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-public class DynamicBeat extends JFrame{
-	
+public class DynamicBeat extends JFrame {
+
 	private Image screenImage;
 	private Graphics screenGraphic;
-	private Image introBackground;
+
+	private ImageIcon exitButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/exitButtonEntered.png"));
+	private ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/exitButtonBasic.png"));
+	
+	private Image introBackground = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
+	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.png")));
+
+	private JButton exitButton = new JButton(exitButtonBasicImage);
+	
+	private int mouseX, mouseY;
 	
 	public DynamicBeat() {
+		setUndecorated(true);
 		setTitle("Dynamic Beat");
 		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		setResizable(false);
-		setLocationRelativeTo(null); //실행 했을 때 컴퓨터의 정중앙으로 뜨도록
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//게임창을 종료 했을 때 프로그램 모두 종료
-		setVisible(true); //창이 보이도록, 출력이 되도록
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setBackground(new Color(0, 0, 0, 0));
+		setLayout(null);
+
+		exitButton.setBounds(1160, 0, 30, 30);
+		exitButton.setBorderPainted(false);
+		exitButton.setContentAreaFilled(false);
+		exitButton.setFocusPainted(false);
+		exitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				exitButton.setIcon(exitButtonEnteredImage);
+				exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonEnteredMusic = new Music("buttonEnteredMusic.mp3", false);
+				buttonEnteredMusic.start();
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				exitButton.setIcon(exitButtonBasicImage);
+				exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Music buttonEnteredMusic = new Music("buttonPressedMusic.mp3", false);
+				buttonEnteredMusic.start();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				}
+				System.exit(0);
+			}
+		});
+		add(exitButton);
 		
-		introBackground = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
-		
+		menuBar.setBounds(0, 0, 1280, 30);
+		menuBar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				mouseX = e.getX();
+				mouseY = e.getY();
+			}
+		});
+		menuBar.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				int x = e.getXOnScreen();
+				int y = e.getYOnScreen();
+				setLocation(x - mouseX, y - mouseY);
+			}
+		});
+		add(menuBar);
+
 		Music introMusic = new Music("introMusic.mp3", true);
 		introMusic.start();
 	}
-	
-	//반복해서 화면을 띄움
+
 	public void paint(Graphics g) {
 		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		screenGraphic = screenImage.getGraphics();
 		screenDraw(screenGraphic);
-		g.drawImage(screenImage,0,0,null);
+		g.drawImage(screenImage, 0, 0, null);
 	}
-	
+
 	public void screenDraw(Graphics g) {
 		g.drawImage(introBackground, 0, 0, null);
-		this.repaint();	//여기서 다시 반복
+		paintComponents(g);
+		this.repaint();
 	}
+
 }
