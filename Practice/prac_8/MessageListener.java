8b6b881ff3e5a0780b1283975fc1a44269f8cd26
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2019-07-21 18:37:06 +0900

    nested interface
    nested interface
    중첩 인터페이스
    버튼 이벤트처리

---
+++ Practice/prac_8/MessageListener.java
@@ -0,0 +1,9 @@
+package Practice;
+
+public class MessageListener implements Button.OnClickListener{
+	@Override
+	public void onClick() {
+		// TODO Auto-generated method stub
+		System.out.println("메세지를 보냅니다.");
+	}
+}
