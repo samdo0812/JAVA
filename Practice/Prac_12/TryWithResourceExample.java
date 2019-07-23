d6d55d8a2c0653250e844fa58fba087d2d69911c
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2019-07-23 15:32:11 +0900

    try-with-resources
    예외처리

---
+++ Practice/Prac_12/TryWithResourceExample.java
@@ -0,0 +1,14 @@
+﻿package Practice;
+
+public class TryWithResourceExample {
+
+	public static void main(String[] args) {
+		try (FileInputStream fis = new FileInputStream("file.txt")){
+			fis.read();
+			throw new Exception();	//강제적으로 예외를 발생시킴
+		} catch (Exception e) {
+			System.out.println("예외 처리 코드가 실행되었습니다.");
+		}
+	}
+
+}
