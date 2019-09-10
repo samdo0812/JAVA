2ed00daed642f6cfeb980eb40c9aef3a61c838da
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2019-09-10 17:42:04 +0900

    클래스, 객체, 참조변수
    클래스, 객체, 참조변수

---
+++ Practice/prac_java/Chapter2/src/Section1/Polynomial.java
@@ -0,0 +1,10 @@
+package Section1;
+
+//하나의 다항식을 표현하기 위한 클래스이다.
+
+public class Polynomial {
+	
+	public char name; //다항식의 이름
+	public int nTerms = 0; //다항식을 구성하는 항의 개수
+	public Term[] terms; //다항식을 구성하는 항들을 저장할 배열
+}
---
+++ Practice/prac_java/Chapter2/src/Section1/Term.java
@@ -0,0 +1,9 @@
+package Section1;
+
+//다항식을 구성하는 하나의 항을 표현하기 위한 클래스
+//계수 coef, 차수 exop를 가진다 
+// 2x^3 
+public class Term {
+	public int coef;
+	public int expo;
+}
