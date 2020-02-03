08d922f9e74dd800badc977e5096406f1f7b00b5
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2020-02-03 22:51:06 +0900

    Scheduler

---
+++ Practice/prac_java/Chapter3/src/section2/Scheduler.java
@@ -0,0 +1,36 @@
+package section2;
+
+import java.util.Scanner;
+
+public class Scheduler {
+	
+	
+	public void processCommand() {
+		Scanner scan = new Scanner(System.in);
+		while(true) {
+			System.out.println("$ ");
+			String command = scan.next();
+			if (command.equals("addevent")) {
+				
+			}
+			else if(command.equals("list")) {
+
+			}
+			else if(command.equals("show")) {
+
+			}
+			else if(command.equals("exit")) {
+				break;
+			}
+		}
+		scan.close();
+	}
+
+	public static void main(String[] args) {
+		
+		Scheduler app = new Scheduler();
+		app.processCommand();
+
+	}
+
+}
