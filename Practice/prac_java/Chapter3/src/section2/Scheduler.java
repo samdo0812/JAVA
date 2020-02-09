9d87b01008a6e9dff8fbcad53805db321dab5d66
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2020-02-09 18:41:32 +0900

    Scheduler

--- Practice/prac_java/Chapter3/src/section2/Scheduler.java
+++ Practice/prac_java/Chapter3/src/section2/Scheduler.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 
 public class Scheduler {
 	
-	public Event[] events = new Event[100];
+	private int capacity = 2;
+	public Event[] events = new Event[capacity];
 	public int n = 0;
 	private Scanner scan;
 	
 	public void processCommand() {
-		Scanner scan = new Scanner(System.in);
+		scan = new Scanner(System.in);
 		while(true) {
 			System.out.print("$ ");
 			String command = scan.next();
@@ -26,7 +27,7 @@ public class Scheduler {
 				}
 			}
 			else if(command.equals("list")) {
-
+				handleList();
 			}
 			else if(command.equals("show")) {
 
@@ -36,6 +37,13 @@ public class Scheduler {
 			}
 		}
 		scan.close();
+	}
+
+	private void handleList() {
+		for (int i = 0; i < n; i++) {
+			System.out.println("  " + events[i].toString());	//dynamic binding
+		}
+		
 	}
 
 	private void handleAddDeadlineEvent() {
@@ -56,11 +64,30 @@ public class Scheduler {
 		
 		MyDate date = parseDateString(dateString);	//dateString의 값을 숫자로만 -> OneDayEvent생성자에서 Mydate타입으로 받음
 		OneDayEvent ev = new OneDayEvent(title, date); 
-		System.out.println(ev.toString());
-		events[n++] = ev; //다형성 확인
+		//System.out.println(ev.toString());
+		
+		addEvent(ev);
+		
 	}
 
 	
+	private void addEvent(OneDayEvent ev) {
+		if(n>=capacity)
+		{
+			reallocate();
+		}
+		events[n++] = ev; //다형성 확인
+	}
+
+	private void reallocate() {
+		Event [] tmp = new Event[capacity * 2];
+		for (int i = 0; i < n; i++) {
+			tmp[i]=events[i];
+		}
+		events = tmp;
+		capacity *= 2;
+	}
+
 	private MyDate parseDateString(String dateString) {
 		String []tokens = dateString.split("/");
 		//tokens[]는 문자열을 받음, Mydate클래스에서는 정수 값으로 받음
